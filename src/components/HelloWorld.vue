5bf2b0fd14308a740dd35ad9bed41904d7d38b72
leonardo1497 <1497enrique@gmail.com>
2019-07-04 02:11:41 -0500

    files to work

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,105 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div style="margin:5%">
+    <v-card>
+    <v-card-title>Historial de compras</v-card-title></v-card>
+    <v-data-table :headers="headers" :items="elements" class="elevation-1">
+      <template v-slot:items="props">
+        <td>{{ props.item.nombre }}</td>
+        <td class="text-xs-left">{{ props.item.pizzaNombre }}</td>
+        <td class="text-xs-left">{{ props.item.correo }}</td>
+        <td class="text-xs-left">{{ props.item.direccion }}</td>
+        <td class="text-xs-left">{{ props.item.telefono }}</td>
+        <td class="text-xs-left">$ {{ props.item.totalPago }}</td>
+        <td class="text-xs-left">{{ props.item.fecha }}</td>
+        <td class="text-xs-left" v-if="props.item.estado == '1'"><v-btn   @click="notificarCliente(props.item.id,props.item.token,props.item.estado)">En proceso</v-btn></td>
+        <td class="text-xs-left" v-if="props.item.estado == '2'"><v-btn  @click="notificarCliente(props.item.id,props.item.token,props.item.estado)">En entrega</v-btn></td>
+        <td class="text-xs-left" v-if="props.item.estado == '3'"><v-btn  @click="notificarCliente(props.item.id,props.item.token,props.item.estado)">Entregado</v-btn></td>
+        <td class="text-xs-left" v-if="props.item.estado == '4'"><v-btn  >Finalizado</v-btn></td>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+<script >
+  export default {
+   data: ()=>({
+     boton:true,
+     boton2: false,
+     boton3: false,
+     elements: [],
+      headers: [  
+         {text:'Comprador',value:'nombre'},
+         { text: 'Pizza', value: 'pizza' },
+        { text: 'Correo', value: 'correo' },
+        { text: 'Dirección', value: 'direccion' },
+        { text: 'Teléfono', value: 'telefono' },
+        { text: 'Total de pago', value: 'totalPago' },
+        { text: 'Fecha', value: 'fecha' },
+        {text: 'Estado', value: 'estado'}
+      ],
+    }),
+    created () {
+      this.getElements()
+    },
+    methods: {
+      getElements () {
+      this.$axios.get(`https://api-pizza-adonis.herokuapp.com/api/v1/ordenes`)
+      .then(response => {
+        console.log(response.data)
+        this.elements = response.data
+      })
+      .catch( e => {
+        console.log(e)
+      })
+    },
+    notificarCliente(id,token,estado){
+      let newEstado;
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
-}
+      let body;
+      if(estado=="1"){
+        body="Tu pizza esta en proceso de realización"
+        newEstado="2"
+      }else if(estado=="2"){
+        body="Tu pizza esta rumbo a su entrega"
+        newEstado="3"
+      }else if(estado=="3"){
+        body="Tu pizza ha sido entregada exitosamente"
+        newEstado="4"
+      }
+      let notification = {
+        "title": "Notificación pizza",
+        "body": body,
+        "icon": "./img/icons/android-chrome-192x192.png"
+      }
+      let data = {
+        "to": token,
+        "notification": notification
+      }
+      this.$axios.post('https://fcm.googleapis.com/fcm/send',data,{
+        headers:{
+          'Authorization': "key=AAAAha0ESNM:APA91bGiMuE_dGDCW0nHcuEE4fD-T0qArbtOlyYqvu2cRhMmg58lMLqriu0EIUcBIqY3OD0q_EQUBLpqYEsm4ClPEbYKENe0jYL-abQ5DvOpQ0vmUDG1OjQGs2j1-_E9wtjtaJ8YklBL",
+          'Content-Type': "application/json"
+
+        },
+
+        
+        }).then((reponse)=>{
+          console.log(reponse)
+        }).catch( (e) => {
+          console.log(e)
+        })
+        this.$axios.put(`https://api-pizza-adonis.herokuapp.com/api/v1/ordenes/`+id,{
+          estado: newEstado,
+      
+
+        }).then((reponse)=>{
+          console.log(reponse);
+          this.getElements();
+         
+        }).catch( (e) => {
+          console.log(e)
+        })
+    }
+    }
+  }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import Vue from 'vue';
+import './plugins/vuetify'
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import axios from 'axios';
 
 Vue.config.productionTip = false;
-
+Vue.prototype.$axios = axios
 new Vue({
   router,
   store,
